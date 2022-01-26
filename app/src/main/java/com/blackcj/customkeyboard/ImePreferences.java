711d77cf74fd0d0fe3246a0a08ad5c33fdd24444
luisboto <luisboto@hotmail.es>
2022-01-26 17:33:33 +0100

    Removed preference activity, crashes

--- app/src/main/java/com/blackcj/customkeyboard/ImePreferences.java
+++ app/src/main/java/com/blackcj/customkeyboard/ImePreferences.java
@@ -15,42 +15,44 @@
  */
 
 package com.blackcj.customkeyboard;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-
+import androidx.preference.PreferenceFragmentCompat;
 import com.android.inputmethodcommon.InputMethodSettingsFragment;
 
 /**
  * Displays the IME preferences inside the input method setting.
  */
-public class ImePreferences extends PreferenceActivity {
+public class ImePreferences extends PreferenceFragmentCompat {
 
     public ImePreferences() {
         super();
     }
 
-    @Override
+    /*@Override
     public Intent getIntent() {
         final Intent modIntent = new Intent(super.getIntent());
         modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
         modIntent.putExtra(EXTRA_NO_HEADERS, true);
         return modIntent;
-    }
+    }*/
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // We overwrite the title of the activity, as the default one is "Voice Search".
-        setTitle(R.string.settings_name);
+        //setTitle(R.string.settings_name);
     }
 
     @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        //super.onCreatePreferences(savedInstanceState, rootKey);
+    }
+
+    /*@Override
     protected boolean isValidFragment(final String fragmentName) {
         return Settings.class.getName().equals(fragmentName);
-    }
+    }*/
 
     public static class Settings extends InputMethodSettingsFragment {
         @Override
