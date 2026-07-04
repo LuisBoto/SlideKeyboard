b926af1c615228a51f847bb2259b88fa3990d088
luisboto <43352462+LuisBoto@users.noreply.github.com>
2026-07-04 20:06:08 +0200

    Landscape layouts improvement

--- app/src/main/java/com/astro/slidekeyboard/KeyboardService.java
+++ app/src/main/java/com/astro/slidekeyboard/KeyboardService.java
@@ -1,39 +1,59 @@
 package com.astro.slidekeyboard;
 import android.content.Context;
 import android.content.res.Configuration;
-import android.os.Build;
+
+import java.util.Map;
 
 public class KeyboardService {
 
     private Context context;
-    private LatinKeyboard mQwertyKeyboard;
-    private LatinKeyboard mLandscapeQwertyKeyboard;
-
-    private LatinKeyboard mSymbolsKeyboard;
-    private LatinKeyboard mEmojiKeyboard;
+    private Map<Keyboards, LatinKeyboard> portraitKeyboards;
+    private Map<Keyboards, LatinKeyboard> landscapeKeyboards;
 
     public KeyboardService(Context context) {
         this.context = context;
-        mQwertyKeyboard = new LatinKeyboard(context, R.xml.qwerty);
-        mSymbolsKeyboard = new LatinKeyboard(context, R.xml.symbols);
-        mEmojiKeyboard = new LatinKeyboard(context, R.xml.emoji);
+        var mQwertyKeyboard = new LatinKeyboard(context, R.xml.qwerty);
+        var mSymbolsKeyboard = new LatinKeyboard(context, R.xml.symbols);
+        var mEmojiKeyboard = new LatinKeyboard(context, R.xml.emoji);
 
-        mLandscapeQwertyKeyboard = new LatinKeyboard(context, R.xml.qwerty_landscape);
+        var mLandscapeQwertyKeyboard = new LatinKeyboard(context, R.xml.qwerty_landscape);
+        var mLandscapeSymbolsKeyboard = new LatinKeyboard(context, R.xml.symbols_landscape);
+        var mLandscapeEmojiKeyboard = new LatinKeyboard(context, R.xml.emoji_landscape);
+
+        portraitKeyboards = Map.of(
+                Keyboards.QWERTY, mQwertyKeyboard,
+                Keyboards.SYMBOL, mSymbolsKeyboard,
+                Keyboards.EMOJI, mEmojiKeyboard);
+
+        landscapeKeyboards = Map.of(
+                Keyboards.QWERTY, mLandscapeQwertyKeyboard,
+                Keyboards.SYMBOL, mLandscapeSymbolsKeyboard,
+                Keyboards.EMOJI, mLandscapeEmojiKeyboard);
     }
 
     public LatinKeyboard getQwertyKeyboard() {
-        return isLandscape() ? this.mLandscapeQwertyKeyboard : this.mQwertyKeyboard;
+        return this.getKeyboards().get(Keyboards.QWERTY);
     }
 
     public LatinKeyboard getEmojiKeyboard() {
-        return this.mEmojiKeyboard;
+        return this.getKeyboards().get(Keyboards.EMOJI);
     }
 
     public LatinKeyboard getSymbolKeyboard() {
-        return this.mSymbolsKeyboard;
+        return this.getKeyboards().get(Keyboards.SYMBOL);
+    }
+
+    private Map<Keyboards, LatinKeyboard> getKeyboards() {
+        return this.isLandscape() ? this.landscapeKeyboards : this.portraitKeyboards;
     }
 
     private boolean isLandscape() {
         return this.context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    private enum Keyboards {
+        QWERTY,
+        SYMBOL,
+        EMOJI;
     }
 }
